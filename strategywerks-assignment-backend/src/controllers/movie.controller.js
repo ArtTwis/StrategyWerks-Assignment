6d6dd1d36729c283b88447d07825183489297ac8
Art_Twis <34947496+ArtTwis@users.noreply.github.com>
2024-08-22 23:34:03 +0530

    modify query parameters

--- strategywerks-assignment-backend/src/controllers/movie.controller.js
+++ strategywerks-assignment-backend/src/controllers/movie.controller.js
@@ -7,9 +7,9 @@ import { Movie } from "../models/movie.model.js";
 
 export const getAllMovies = asyncHandler(async (req, res) => {
   try {
-    const page = Number(req.query.page) || 1;
+    const page = Number(req.query._page) || 1;
 
-    const limit = Number(req.query.limit) || 10;
+    const limit = Number(req.query._limit) || 6;
 
     const skip = (page - 1) * limit;
 
